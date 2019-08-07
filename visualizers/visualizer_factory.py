0f8e7a9bb5d900648e60838a2f4a2867cfba6c7e
wonkyunglee <leewk921223@gmail.com>
2019-08-07 18:47:13 +0000

    fix visualizer bug : change minus to abs(minus)

--- visualizers/visualizer_factory.py
+++ visualizers/visualizer_factory.py
@@ -34,6 +34,7 @@ def get_figure_basic(LR, HR, pred):
     HR = HR[0]
     pred_residual_hr = pred['residual_hr'][0]
     pred_hr = pred['hr'][0]
+    gt_diff = torch.abs(HR - pred_hr)
 
     fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(16,4))
     cmap = 'gray'
@@ -43,7 +44,7 @@ def get_figure_basic(LR, HR, pred):
     ax2.set_title('pred_hr, mean_val : %.4f'%pred_hr.mean())
     ax3.imshow(float2uint8(HR), cmap=cmap)
     ax3.set_title('ground_truth, mean_val : %.4f'%HR.mean())
-    ax4.imshow(float2uint8(HR - pred_hr), cmap=cmap)
+    ax4.imshow(float2uint8(gt_diff), cmap=cmap)
     ax4.set_title('GT - pred_hr, mean_val : %.4f'%(HR-pred_hr).mean())
 
     return fig
@@ -64,7 +65,8 @@ def step2_visualizer(rgb_range):
         pred_student_residual_hr = pred_student['residual_hr'][0]
         pred_student_hr = pred_student['hr'][0]
         pred_teacher_hr = pred_teacher['hr'][0]
-        residual_diff = pred_teacher_residual_hr - pred_student_residual_hr
+        residual_diff = torch.abs(pred_teacher_residual_hr - pred_student_residual_hr)
+        gt_diff = torch.abs(HR - pred_student_hr)
 
         fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(16,4))
         cmap = 'gray'
@@ -74,8 +76,8 @@ def step2_visualizer(rgb_range):
         ax2.set_title('pred_s_hr, mean_val : %.4f'%pred_student_hr.mean())
         ax3.imshow(float2uint8(quantize(residual_diff, rgb_range), rgb_range), cmap=cmap)
         ax3.set_title('residual_diff, mean_val : %.4f'%residual_diff.mean())
-        ax4.imshow(float2uint8(quantize(HR - pred_student_hr, rgb_range), rgb_range), cmap=cmap)
-        ax4.set_title('GT - pred_hr, mean_val : %.4f'%(HR-pred_student_hr).mean())
+        ax4.imshow(float2uint8(quantize(gt_diff, rgb_range), rgb_range), cmap=cmap)
+        ax4.set_title('GT - pred_hr, mean_val : %.4f'%(gt_diff).mean())
 
         return fig
 
@@ -90,7 +92,8 @@ def step2_attention_visualizer(rgb_range):
         pred_student_residual_hr = pred_student['residual_hr'][0]
         pred_student_hr = pred_student['hr'][0]
         pred_teacher_hr = pred_teacher['hr'][0]
-        residual_diff = pred_teacher_residual_hr - pred_student_residual_hr
+        residual_diff = torch.abs(pred_teacher_residual_hr - pred_student_residual_hr)
+        gt_diff = torch.abs(HR - pred_student_hr)
         attention = pred_student['mapping_attention'][0]
 
         fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(1, 5, figsize=(15,3))
@@ -101,7 +104,7 @@ def step2_attention_visualizer(rgb_range):
         ax2.set_title('pred_s_hr, mean_val : %.4f'%pred_student_hr.mean())
         ax3.imshow(float2uint8(quantize(residual_diff, rgb_range), rgb_range), cmap=cmap)
         ax3.set_title('residual_diff, mean_val : %.4f'%residual_diff.mean())
-        ax4.imshow(float2uint8(quantize(HR - pred_student_hr, rgb_range), rgb_range), cmap=cmap)
+        ax4.imshow(float2uint8(quantize(gt_diff, rgb_range), rgb_range), cmap=cmap)
         ax4.set_title('GT - pred_hr, mean_val : %.4f'%(HR-pred_student_hr).mean())
         ax5.imshow(float2uint8(quantize(attention, rgb_range), rgb_range), cmap=cmap)
         ax5.set_title('attention, mean_val : %.4f'%(attention).mean())
