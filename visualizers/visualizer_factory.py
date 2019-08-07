5501e2a890732fe5d8ca3df3d11f22f2bbb3b1f0
wonkyunglee <leewk921223@gmail.com>
2019-08-07 17:39:08 +0000

    fix visualizer bug

--- visualizers/visualizer_factory.py
+++ visualizers/visualizer_factory.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append('../')
 from utils.utils import quantize
 
-def float2uint8(image, normalize=False):
+def float2uint8(image, normalize=False, rgb_range=1):
     if type(image) == torch.Tensor:
         image = image.detach().cpu().numpy()
     if len(image.shape) == 4:
@@ -20,7 +20,7 @@ def float2uint8(image, normalize=False):
         image += min_val
         max_val = np.max(image)
         image /= max_val
-        image *= 255
+    image *= 255/rgb_range
     image = image.astype('uint8')
     if image.shape[-1] != 1:
         image = np.transpose(image, (1,2,0))
@@ -68,13 +68,13 @@ def step2_visualizer(rgb_range):
 
         fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(16,4))
         cmap = 'gray'
-        ax1.imshow(float2uint8(quantize(pred_student_residual_hr, rgb_range)), cmap=cmap)
+        ax1.imshow(float2uint8(quantize(pred_student_residual_hr, rgb_range), rgb_range), cmap=cmap)
         ax1.set_title('pred_s_residual_hr, mean_val : %.4f'% pred_student_residual_hr.mean())
-        ax2.imshow(float2uint8(quantize(pred_student_hr, rgb_range)), cmap=cmap)
+        ax2.imshow(float2uint8(quantize(pred_student_hr, rgb_range), rgb_range), cmap=cmap)
         ax2.set_title('pred_s_hr, mean_val : %.4f'%pred_student_hr.mean())
-        ax3.imshow(float2uint8(quantize(residual_diff, rgb_range)), cmap=cmap)
+        ax3.imshow(float2uint8(quantize(residual_diff, rgb_range), rgb_range), cmap=cmap)
         ax3.set_title('residual_diff, mean_val : %.4f'%residual_diff.mean())
-        ax4.imshow(float2uint8(quantize(HR - pred_student_hr, rgb_range)), cmap=cmap)
+        ax4.imshow(float2uint8(quantize(HR - pred_student_hr, rgb_range), rgb_range), cmap=cmap)
         ax4.set_title('GT - pred_hr, mean_val : %.4f'%(HR-pred_student_hr).mean())
 
         return fig
@@ -95,15 +95,15 @@ def step2_attention_visualizer(rgb_range):
 
         fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(1, 5, figsize=(15,3))
         cmap = 'gray'
-        ax1.imshow(float2uint8(quantize(pred_student_residual_hr, rgb_range)), cmap=cmap)
+        ax1.imshow(float2uint8(quantize(pred_student_residual_hr, rgb_range), rgb_range), cmap=cmap)
         ax1.set_title('pred_s_residual_hr, mean_val : %.4f'% pred_student_residual_hr.mean())
-        ax2.imshow(float2uint8(quantize(pred_student_hr, rgb_range)), cmap=cmap)
+        ax2.imshow(float2uint8(quantize(pred_student_hr, rgb_range), rgb_range), cmap=cmap)
         ax2.set_title('pred_s_hr, mean_val : %.4f'%pred_student_hr.mean())
-        ax3.imshow(float2uint8(quantize(residual_diff, rgb_range)), cmap=cmap)
+        ax3.imshow(float2uint8(quantize(residual_diff, rgb_range), rgb_range), cmap=cmap)
         ax3.set_title('residual_diff, mean_val : %.4f'%residual_diff.mean())
-        ax4.imshow(float2uint8(quantize(HR - pred_student_hr, rgb_range)), cmap=cmap)
+        ax4.imshow(float2uint8(quantize(HR - pred_student_hr, rgb_range), rgb_range), cmap=cmap)
         ax4.set_title('GT - pred_hr, mean_val : %.4f'%(HR-pred_student_hr).mean())
-        ax5.imshow(float2uint8(quantize(attention, rgb_range)), cmap=cmap)
+        ax5.imshow(float2uint8(quantize(attention, rgb_range), rgb_range), cmap=cmap)
         ax5.set_title('attention, mean_val : %.4f'%(attention).mean())
         return fig
 
