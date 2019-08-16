89bfb327564ae8a6d1f0fcc197c3bde073cb7122
wonkyunglee <leewk921223@gmail.com>
2019-08-16 09:05:52 +0000

    add noisy teacher model

--- models/model_factory.py
+++ models/model_factory.py
@@ -6,6 +6,9 @@ import sys
 sys.path.append('../')
 from utils.checkpoint import get_last_checkpoint
 
+import os
+device = None
+
 
 class ModifiedFSRCNN(nn.Module):
     def __init__(self, scale, n_colors, d=56, s=12, m_1=4, m_2=3, dilation=1):
@@ -287,6 +290,58 @@ class AttendSimilarityStudentNet(BaseNet):
         return ret_dict
 
 
+class NoisyTeacherNet(BaseNet):
+    def __init__(self, scale, n_colors, d=56, s=12, m_1=4, m_2=3,layers_to_attend=None, modules_to_freeze=None,
+                 initialize_from=None, modules_to_initialize=None, dilation=1):
+        super(NoisyTeacherNet, self).__init__()
+
+        self.layers_to_attend = layers_to_attend if layers_to_attend is not None else []
+        self.scale = scale
+        upscale_factor = scale
+
+        self.initialize_from = initialize_from
+        self.modules_to_freeze = modules_to_freeze
+        self.modules_to_initialize = modules_to_initialize
+
+        self.backbone = ModifiedFSRCNN(scale, n_colors, d, s, m_1, m_2, dilation)
+        self.weight_init()
+        if initialize_from is not None:
+            self.load_pretrained_model()
+        if modules_to_freeze is not None:
+            self.freeze_modules()
+
+
+    def get_cos_similarity(self, x, y):
+        dist = F.cosine_similarity(x, y, dim=1).unsqueeze(1)
+        dist = (dist + 1.0) / 2.01
+        return dist
+
+
+    def forward(self, LR, HR, student_pred_dict=None):
+        ret_dict = dict()
+        upscaled_lr = nn.functional.interpolate(LR, scale_factor=self.scale, mode='bicubic')
+        x = HR
+
+        layer_names = self.backbone.network._modules.keys()
+        for layer_name in layer_names:
+            if student_pred_dict is not None and layer_name in self.layers_to_attend:
+                teacher_x = self.backbone.network._modules[layer_name](x)
+                student_x = student_pred_dict[layer_name]
+                cos_sim = self.get_cos_similarity(student_x, teacher_x).detach()
+                std = (1 - cos_sim) * 100
+                x = teacher_x + torch.randn_like(teacher_x).to(device) * std
+            else:
+                x = self.backbone.network._modules[layer_name](x)
+            ret_dict[layer_name] = x
+
+        residual_hr = x
+        hr = upscaled_lr + residual_hr
+        ret_dict['hr'] = hr
+        ret_dict['residual_hr'] = residual_hr
+
+        return ret_dict
+
+
 # For Resolution Disentangling Experiments
 def get_disentangle_student(scale, n_colors, **kwargs):
     return DisentangleStudentNet(scale, n_colors, **kwargs)
@@ -304,7 +359,16 @@ def get_attend_similarity_student(scale, n_colors, **kwargs):
     return AttendSimilarityStudentNet(scale, n_colors, **kwargs)
 
 
+def get_noisy_teacher(scale, n_colors, **kwargs):
+    return NoisyTeacherNet(scale, n_colors, **kwargs)
+
+
 def get_model(config, model_type):
+
+    global device
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(config.gpu)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     print('model name:', config[model_type+'_model'].name)
 
     f = globals().get('get_' + config[model_type+'_model'].name)
